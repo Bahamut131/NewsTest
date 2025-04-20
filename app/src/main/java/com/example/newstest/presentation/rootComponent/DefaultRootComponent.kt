f6d9780ada72c362d18d38382ca483dd8ac53157
Bahamut131 <sotnikov.aleksey.2020@gmail.com>
2025-04-20 21:42:42 +0300

    Add an implementation of the root component that is responsible for the application navigation graph

--- app/src/main/java/com/example/newstest/presentation/rootComponent/DefaultRootComponent.kt
+++ app/src/main/java/com/example/newstest/presentation/rootComponent/DefaultRootComponent.kt
@@ -1,9 +1,72 @@
 package com.example.newstest.presentation.rootComponent
 
+import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.push
 import com.arkivanov.decompose.value.Value
+import com.example.newstest.presentation.categoryScreen.DefaultCategoryComponent
+import com.example.newstest.presentation.homeScreen.DefaultHomeComponent
+import com.example.newstest.presentation.rootComponent.RootComponent.Child
+import com.example.newstest.presentation.rootComponent.RootComponent.Child.CategoryComponentContent
+import com.example.newstest.presentation.rootComponent.RootComponent.Child.HomeComponentContent
+import dagger.assisted.AssistedInject
+import kotlinx.serialization.Serializable
 
-class DefaultRootComponent : RootComponent {
-    override val stack: Value<ChildStack<*, RootComponent.Child>>
-        get() = TODO("Not yet implemented")
+class DefaultRootComponent @AssistedInject constructor(
+    private val defaultHomeComponent: DefaultHomeComponent.Factory,
+    private val defaultCategoryComponent: DefaultCategoryComponent.Factory,
+    private val componentContext: ComponentContext
+) : RootComponent, ComponentContext by componentContext{
+
+    private val navigation = StackNavigation<Config>()
+    override val stack: Value<ChildStack<*, Child>> = childStack(
+        source = navigation,
+        initialConfiguration = Config.HomeScreenConfig,
+        handleBackButton = true,
+        childFactory = ::child,
+        serializer = Config.serializer()
+    )
+
+    private fun child(
+        config: Config,
+        componentContext: ComponentContext,
+    ) : Child{
+        when(config){
+            Config.CategoryScreenConfig -> {
+
+                val component = defaultCategoryComponent.create(
+                    componentContext = componentContext,
+                    onClickBack = { navigation.pop() }
+                )
+
+                return CategoryComponentContent(component)
+            }
+            Config.HomeScreenConfig -> {
+                val component = defaultHomeComponent.create(
+                    componentContext = componentContext,
+                    onClickBack = {navigation.pop()}
+                )
+                return HomeComponentContent(component)
+            }
+
+        }
+    }
+
+
+    fun navigateToHome() {
+        navigation.push(Config.HomeScreenConfig)
+    }
+
+    fun navigateToCategory() {
+        navigation.push(Config.CategoryScreenConfig)
+    }
+
+    @Serializable
+    sealed interface Config{
+        data object HomeScreenConfig : Config
+        data object CategoryScreenConfig : Config
+    }
 }
