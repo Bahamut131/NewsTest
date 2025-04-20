3acc25d471ae08fe69f59d0d90e7d143be0c4329
Bahamut131 <sotnikov.aleksey.2020@gmail.com>
2025-04-20 21:39:42 +0300

    Add implementation of category screen

--- app/src/main/java/com/example/newstest/presentation/categoryScreen/DefaultCategoryComponent.kt
+++ app/src/main/java/com/example/newstest/presentation/categoryScreen/DefaultCategoryComponent.kt
@@ -1,20 +1,58 @@
 package com.example.newstest.presentation.categoryScreen
 
 import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.mvikotlin.core.instancekeeper.getStore
+import com.arkivanov.mvikotlin.extensions.coroutines.labels
+import com.arkivanov.mvikotlin.extensions.coroutines.stateFlow
+import com.example.newstest.presentation.core.componentScope
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
-class DefaultCategoryComponent(
-    private val store: CategoryStore,
-    private val componentContext: ComponentContext
+class DefaultCategoryComponent @AssistedInject constructor(
+    private val store: CategoryStoreFactory,
+    @Assisted("componentContext")private val componentContext: ComponentContext,
+    @Assisted("onClickBack")private val onClickBack : () -> Unit
 ) : CategoryComponent, ComponentContext by componentContext{
+    private val state = componentContext.instanceKeeper.getStore { store.create() }
+    @OptIn(ExperimentalCoroutinesApi::class)
     override val model: StateFlow<CategoryStore.State>
-        get() = TODO("Not yet implemented")
+        get() = state.stateFlow
+
+    init {
+        componentScope().launch {
+            state.labels.collect {
+                when(it){
+                    CategoryStore.Label.OnBackClick -> onClickBack.invoke()
+                }
+            }
+        }
+    }
 
     override fun loadNewsByCategory(category: String) {
-        TODO("Not yet implemented")
+        state.accept(CategoryStore.Intent.ChangeCategory(category))
     }
 
     override fun loadCategoryNext() {
-        TODO("Not yet implemented")
+        state.accept(CategoryStore.Intent.LoadNextNews)
+    }
+
+    override fun onClickBack() {
+        state.accept(CategoryStore.Intent.OnBackClick)
+    }
+
+    override fun onClickSearchNews() {
+        state.accept(CategoryStore.Intent.OnSearchClick)
+    }
+
+    @AssistedFactory
+    interface Factory{
+        fun create(
+            @Assisted("componentContext") componentContext: ComponentContext,
+            @Assisted("onClickBack") onClickBack : () -> Unit
+        ) : DefaultCategoryComponent
     }
 }
---
+++ app/src/main/java/com/example/newstest/presentation/categoryScreen/category/CategoryName.kt
@@ -0,0 +1,11 @@
+package com.example.newstest.presentation.categoryScreen.category
+
+enum class CategoryName(val category : String) {
+    BUSINESS("business"),
+    ENTERTAINMENT("entertainment"),
+    GENERAL("general"),
+    HEALTH("health"),
+    SCIENCE("science"),
+    SPORTS("sports"),
+    TECHNOLOGY("technology")
+}
